3b2484807df1ac2350e178f7427cfd668fca212f
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-05-04 12:05:50 -0700

    Imported form and style

--- react-postit-basics/src/components/PostsList.jsx
+++ react-postit-basics/src/components/PostsList.jsx
@@ -1,10 +1,14 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 export default function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post fighter="Dustin" quote="Don't be silly, jump the gilly" />
-      <Post fighter="Islam" quote="Kangaroo have no wrestling brether" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post fighter="Dustin" quote="Don't be silly, jump the gilly" />
+        <Post fighter="Islam" quote="Kangaroo have no wrestling brether" />
+      </ul>
+    </>
   );
 }
