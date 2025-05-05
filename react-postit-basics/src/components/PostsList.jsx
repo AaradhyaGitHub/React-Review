f83ec739f6bbad9a7a8e27f930fe47af861d3218
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-05-04 17:38:53 -0700

    Added form submission and validation

--- react-postit-basics/src/components/PostsList.jsx
+++ react-postit-basics/src/components/PostsList.jsx
@@ -2,29 +2,13 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import Modal from "./Modal";
-import { useState } from "react";
 
-export default function PostsList({isPosting, onStopPosting}) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
- 
-
-  const changeBodyHandler = (event) => {
-    setEnteredBody(event.target.value);
-  };
-  const authorChangeHandler = (event) => {
-    setEnteredAuthor(event.target.value);
-  };
-
+export default function PostsList({ isPosting, onStopPosting }) {
   let modalContent;
   if (isPosting) {
     modalContent = (
       <Modal onClose={onStopPosting}>
-        <NewPost
-          onBodyChange={changeBodyHandler}
-          onAuthorChange={authorChangeHandler}
-        />
+        <NewPost onCancel={onStopPosting} />
       </Modal>
     );
   }
@@ -33,8 +17,7 @@ export default function PostsList({isPosting, onStopPosting}) {
     <>
       {modalContent}
       <ul className={classes.posts}>
-        <Post fighter={enteredAuthor} quote={enteredBody} />
-        <Post fighter="Islam" quote="Kangaroo have no wrestling brether" />
+        <Post fighter="Dustin" quote="don't be silly jump the gilly" />
       </ul>
     </>
   );
